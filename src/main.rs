f17159464f9eeaa4cb01b2f551b68369a480af96
DanielThomas44123 <yaleoggrads@gmail.com>
2024-09-10 14:13:57 -0500

    Added find intersection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 mod types;
+use std::collections::btree_set::Intersection;
+
 use crate::types::*;
 fn main() {
     println!("Hello, world!");
     let testeq:LinearSlopeInterceptForm = LinearSlopeInterceptForm {
-        m: 1.0,
-        b: 0.0,
+        m: 4.0,
+        b: 4.0,
     };
-    
+    let testeq2:LinearSlopeInterceptForm = LinearSlopeInterceptForm {
+        m: 2.0,
+        b: 3.0,
+    };
+    let points:Vec<Point> = testeq.createTable(1.0, vec![-5.0, 5.0]);
+    printVecOfPoints(&points);
+    Point{
+        x: 0.0,
+        y: 0.2,
+    }.logSelf();
+    let Intersection:Point = findIntersection(&testeq, &testeq2);
+    Intersection.logSelf();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,17 +1,30 @@
+use std::ptr::eq;
 
 pub struct Point {
   pub x: f64,
   pub y: f64,
 }
-pub struct LinearSlopeInterceptForm {
+pub struct LinearSlopeInterceptForm { // y = mx + b
   pub m: f64,
   pub b: f64,
 }
-pub struct QuadradicStandardForm {
+pub struct QuadradicStandardForm { //y = ax^2 + bx + c
   pub a: f64,
   pub b: f64,
   pub c: f64,
 }
+impl Point {
+  pub fn logSelf(&self) -> () {
+    print!("(");
+    print!("{}", self.x);
+    print!(", ");
+    print!("{}", self.y);
+    println!(")");
+  }
+}
+
+
+
 impl LinearSlopeInterceptForm {
   pub fn xIntercept(&self) -> Point {
     let xValue:f64 = (self.b * -1.0) / self.m;
@@ -45,5 +58,47 @@ impl LinearSlopeInterceptForm {
       i += granularity;
     }
     return points;
+  }
+}
+
+
+
+pub fn printVecOfPoints(points:&Vec<Point>) -> () {
+let mut i:usize = 0;
+println!("[");
+while i < points.len() {
+  print!("    ");
+  points[i].logSelf();
+  i += 1;
+}
+println!("]")
+}
+
+pub fn testForIntersection(eq_one: &LinearSlopeInterceptForm, eq_two:&LinearSlopeInterceptForm) -> bool {
+if eq_one.m == eq_two.m { //They are parallel or indistinct.
+  return false;
+} else { //If the slopes are different, they are not parallel,
+  return true; //so they will intersect
+}
+}
+
+pub fn findIntersection(eq_one: &LinearSlopeInterceptForm, eq_two:&LinearSlopeInterceptForm) -> Point { // test more, not sure this really works
+  if eq_one.b == eq_two.b {
+    return Point {
+      x: 0.0,
+      y: 0.0,
+    }
+  } else {
+    let equalityCoeffecient:f64 = eq_one.m / eq_two.m; //Potential divide by zero
+    let eq_two_modified:LinearSlopeInterceptForm = LinearSlopeInterceptForm {
+      m: eq_two.m *-1.0 * equalityCoeffecient,
+      b: eq_two.b *-1.0 * equalityCoeffecient,
+    };
+    let solved_y:f64 = eq_one.b + eq_two_modified.b;
+    let solved_x:f64 = eq_one.solveForXValue(solved_y).x;
+    return Point{
+      x: solved_x,
+      y: solved_y,
+    }
   }
 }
